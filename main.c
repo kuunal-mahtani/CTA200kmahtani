ad6337c623a324fc542ce80f907ec4846294acd9
kmahtani <k.mahtani@mail.utoronto.ca>
2020-05-06 17:41:08 -0400

    edited main.c file

--- main.c
+++ main.c
@@ -3,11 +3,14 @@
 
 
 int main() {
-    double arr[] = {1.0, 2.0, 3.0, 4.0};
+    double arr[] = {1.0, 2.0, 3.0, 4.0, 420}; //Added a fifth number '420'
+    
+    int arrlen = 0;
+    arrlen = sizeof(arr)/sizeof(arr[0]); //added an integer that determines length of the array
 
-    double result = average(4, arr);
+    double result = average(arrlen, arr); //calculate average of hard-coded list in a less hard-coded way
 
-    printf("The average of 1, 2, 3 and 4 is: %.4f\n", result);
+    printf("The average of the hard coded list of numbers is: %.4f\n", result); //updated printed description to match what new code does
     return 0;    
 }
 
